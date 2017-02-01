333332818f8afe8f4da1a9a826573d735a968018
D Cameron Mauch <cmauch@enbala.com>
2017-02-01 15:16:09 -0700

    completed with example and tests

--- with/lib/with.ex
+++ with/lib/with.ex
@@ -1,2 +1,67 @@
 defmodule With do
+  @moduledoc """
+  Examples of using the with form
+  """
+
+  @compile if Mix.env == :test, do: :export_all
+
+  @doc """
+  Example to parse list of string data into dates and values
+  """
+
+  @spec process([String.t]) :: [{DateTime.t, float}] | {:error, String.t}
+  def process(data) do
+    with \
+      indexed_data = index(data),
+      parts when is_list(parts) <- split(indexed_data),
+      epochs when is_list(epochs) <- epochs(parts),
+      values when is_list(values) <- values(parts),
+      combined = List.zip([epochs, values])
+    do
+      combined
+    else
+      {:not_slittable, number} -> {:error, "not splittable on line #{number}"}
+      {:not_datetime, number} -> {:error, "invalid datetime on line #{number}"}
+      {:not_value, number} -> {:error, "invalid value on line #{number}"}
+    end
+  end
+
+  defp index([]), do: []
+  defp index(data) do
+    count = Enum.count(data)
+    indexes = Range.new(1, count) |> Enum.to_list
+    List.zip([indexes, data])
+  end
+
+  defp split(indexed_data), do: split(indexed_data, [])
+  defp split([], accumulator), do: accumulator
+  defp split([{number, text} | tail], accumulator) do
+    result = String.split(text, "|") |> Enum.map(&String.trim/1)
+    case result do
+      [left, right] -> split(tail, accumulator ++ [{number, left, right}])
+      _ -> {:not_slittable, number}
+    end
+  end
+
+  def epochs(parts), do: epochs(parts, [])
+  def epochs([], accumulator), do: accumulator
+  def epochs([{number, text, _} | tail], accumulator) do
+    result = DateTime.from_iso8601(text)
+    case result do
+      {:ok, datetime, 0} ->
+        epoch = DateTime.to_unix(datetime)
+        epochs(tail, accumulator ++ [epoch])
+      _ -> {:not_datetime, number}
+    end
+  end
+
+  def values(parts), do: values(parts, [])
+  def values([], accumulator), do: accumulator
+  def values([{number, _, text} | tail], accumulator) do
+    result = Float.parse(text)
+    case result do
+      {value, ""} -> values(tail, accumulator ++ [value])
+      _ -> {:not_value, number}
+    end
+  end
 end
--- with/test/with_test.exs
+++ with/test/with_test.exs
@@ -1,7 +1,89 @@
 defmodule WithTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "process/1" do
+    test "valid data" do
+      data = ["2016-01-01T00:00:00Z|1.1", "2016-01-02T01:02:03Z|2.2"]
+      expected = [{1451606400, 1.1}, {1451696523, 2.2}]
+      assert With.process(data) == expected
+    end
+
+    test "unsplittable line" do
+      data = ["2016-01-01T00:00:00Z|1.1", "2016-01-02T01:02:03Z|2.2|something"]
+      expected = {:error, "not splittable on line 2"}
+      assert With.process(data) == expected
+    end
+
+    test "invalid datetime" do
+      data = ["2016-01-01T00:00:00Z|1.1", "something|2.2"]
+      expected = {:error, "invalid datetime on line 2"}
+      assert With.process(data) == expected
+    end
+
+    test "invalid value" do
+      data = ["2016-01-01T00:00:00Z|1.1", "2016-01-02T01:02:03Z|something"]
+      expected = {:error, "invalid value on line 2"}
+      assert With.process(data) == expected
+    end
+  end
+
+  describe "index/1" do
+    test "successful index" do
+      data = ["foo", "bar"]
+      expected = [{1, "foo"}, {2, "bar"}]
+      assert With.index(data) == expected
+    end
+
+    test "empty list" do
+      assert With.index([]) == []
+    end
+  end
+
+  describe "split/1" do
+    test "successful split" do
+      data = [{1, "abc|123"}, {2, "def|456"}]
+      expected = [{1, "abc", "123"}, {2, "def", "456"}]
+      assert With.split(data) == expected
+    end
+
+    test "successful split with spaces" do
+      data = [{1, " abc | 123 "}]
+      expected = [{1, "abc", "123"}]
+      assert With.split(data) == expected
+    end
+
+    test "with unsplitable line" do
+      data = [{1, "abc|123"}, {2, "foobar"}]
+      expected = {:not_slittable, 2}
+      assert With.split(data) == expected
+    end
+  end
+
+  describe "epochs/1" do
+    test "successful epochs" do
+      data = [{1, "2016-01-01T00:00:00Z", "foo"}, {2, "2016-01-02T01:02:03Z", "bar"}]
+      expected = [1451606400, 1451696523]
+      assert With.epochs(data) == expected
+    end
+
+    test "invalid datetime" do
+      data = [{1, "2016-01-01T00:00:00Z", "something"}, {2, "foo", "bar"}]
+      expected = {:not_datetime, 2}
+      assert With.epochs(data) == expected
+    end
+  end
+
+  describe "values/1" do
+    test "successful values" do
+      data = [{1, "foo", "1.1"}, {2, "bar", "2.2"}]
+      expected = [1.1, 2.2]
+      assert With.values(data) == expected
+    end
+
+    test "invalid value" do
+      data = [{1, "foo", "1.1"}, {2, "bar", "something"}]
+      expected = {:not_value, 2}
+      assert With.values(data) == expected
+    end
   end
 end
